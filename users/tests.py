3734cbffb6f53f601382814001246354f8b93872
Rich Hart <richard.hart@capco.com>
2021-01-17 15:23:06 -0500

    add user serializers and views

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,20 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-class TestProfile(TestCase):
-    def test_create(self):
-        user = User.objects.create()
-        self.assertTrue(user.profile)
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+class TestProfile(APITestCase):
+    def setUp(self):
+        self.url = reverse('profile-list')
+        self.user = User.objects.create_user(
+            username='test_user',
+            email='u@d.com',
+            password='password',
+        )
+    def test_signal(self):
+        self.assertTrue(self.user.profile)
+
+    def test_anonymous_view(self):
+        response = self.client.get(self.url, format='json')
+        self.assertFalse(response.data)
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
+from django.contrib.auth.models import AnonymousUser
+from .serializers import *
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    def get_queryset(self):
+        user = self.request.user
+        if isinstance(user, AnonymousUser):
+            return []
+        return Profile.objects.filter(owner=user)
+    
