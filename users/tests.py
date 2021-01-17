2c653b9e6b642fbc56f27ac4aed1cc19726b5ae0
Rich Hart <richard.hart@capco.com>
2021-01-17 15:47:15 -0500

    add pagination

--- users/tests.py
+++ users/tests.py
@@ -4,6 +4,19 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.test import force_authenticate
+class TestUser(APITestCase):
+    def setUp(self):
+        self.url = reverse('profile-list')
+        self.user = User.objects.create_user(
+            username='test_user',
+            email='u@d.com',
+            password='password',
+        )
+
+    def test_view(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url, format='json')
+        self.assertTrue(response.data['count'])
 
 class TestProfile(APITestCase):
     def setUp(self):
@@ -18,10 +31,10 @@ class TestProfile(APITestCase):
 
     def test_anonymous_view(self):
         response = self.client.get(self.url, format='json')
-        self.assertFalse(response.data)
+        self.assertFalse(response.data['count'])
 
     def test_view(self):
         self.client.force_login(self.user)
         response = self.client.get(self.url, format='json')
-        self.assertTrue(response.data)
+        self.assertTrue(response.data['count'])
 
